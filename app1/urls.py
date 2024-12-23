2287d54ff4e19c97162b1302eb05e267ddaf3276
monu-4042 <g.mohan8096@gmail.com>
2024-12-23 12:28:20 +0530

    hellooo

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+app_name='app1'
+urlpatterns=[
+    path('',home,name='home'),
+    path('contact/',contact,name='contact'),
+    path('bootcamp/',bootcamp,name='bootcamp'),
+    path('signup/',signup,name='signup'),
+]
---
+++ app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def bootcamp(request):
+    return render(request,'bootcamp.html')
+
+def signup(request):
+    return render(request,'signup.html')
